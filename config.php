b751a47fcfcf94c1211b80083ab78a2d2e5b8658
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2020-08-17 05:55:27 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$UserAgent = "Mozilla/5.0 (Linux; Android 5.1.1; SM-T285) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Safari/537.36";
+AllCookie = "__cfduid=d51243ccec3b898f72a4f647ab911233c1597598689;_ga=GA1.2.393394226.1597598693;_luid=1514235;_gid=GA1.2.636325438.1597598693;_lrtk=usqZDFIEUWKbsyB1Z3I4KD4llL4tLX8qGGBhlK3Jbb6LHNq9SrGmX6fhsjcdKd1X";
+?>
